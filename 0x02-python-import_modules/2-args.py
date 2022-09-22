ac4519f8581e80c90b106e2f1550eced1388db4c
Ajayi Timileyin <ajayitimmy45@gmail.com>
2022-09-22 14:40:46 -0700

    second

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
+    i = len(sys.argv) - 1
 
-    n = len(sys.argv)
-    if n == 1:
-        print("0 arguments.")
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(n - 1))
+        print("{} arguments:".format(i))
 
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i]))
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
