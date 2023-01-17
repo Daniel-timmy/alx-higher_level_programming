dee8effb82ea8f31ffb4288ca71fbd597383ea78
Daniel-timmy <ajayitimmy45@gmail.com>
2023-01-17 17:56:57 +0100

    hbtn_status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    page_reply = response.read()
