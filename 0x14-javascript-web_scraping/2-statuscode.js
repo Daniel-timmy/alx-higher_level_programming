428e31bb767b0ab6744093228a253f03833f316e
Ajayi Timileyin <ajayitimmy45@gmail.com>
2023-02-09 20:24:04 -0800

    js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
 
-const request = require('request')
-
-const url = process.argv[2]
-
-request(url, (err, response, body) => {
+request.get(url, (err, response, body) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     } else {
         console.log("code: " + response.statusCode);
     }
-})
+});
